d63ccb85902a3602b2eecb9ec12d3b331c69d51f
jesus <bep010795@gmail.com>
2023-02-19 16:23:43 -0600

    Status code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const routerApi = require('./routes/index')
 const app = express();
 const port =  3000
 
+app.use(express.json())
 routerApi(app)
 
 app.get("/", (req, res) => {
--- routes/products.router.js
+++ routes/products.router.js
@@ -21,12 +21,48 @@ router.get("/", (req, res) => {
 // ---- Get a product
 router.get("/:id", (req, res) => {
     const { id } = req.params;
-    res.json({
-        id,
-        name: "Produc 1",
-        price: 1000
-    });
+    if (id === "999"){
+        res.status(404).json(
+            {
+                massage: "Product not found"
+            }
+        )
+    } else {
+        res.json({
+            id,
+            name: "Produc 1",
+            price: 1000
+        });
+    }
 })
 
+// ---- Create a product
+router.post('/', (req, res) => {
+    const dataBody = req.body;
+    res.status(201).json({
+        messaje: "Created product",
+        data: dataBody
+    })
+})
+
+// ---- Update product
+router.patch("/:id", (req, res) => {
+    const { id } = req.params;
+    const data = req.body;
+    res.json({
+        message: "Update product",
+        data: data,
+        id
+    })
+})
+
+// ---- Delete product
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    res.json({
+        message: "Delete product",
+        id
+    })
+})
 
 module.exports = router;
