8d6428d1dc846c451a719d89fe85453e06bb334e
jesus <bep010795@gmail.com>
2023-02-18 18:28:28 -0600

    Primeros endpoints

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("my-app");
+const express = require('express');
+const app = express();
+const port =  3000
+
+app.get("/", (req, res) => {
+    res.send("Primer servidor en express");
+})
+
+app.get("/nueva-ruta", (req, res) => {
+    res.send("Otra ruta");
+})
+
+app.get("/enviando-json", (req, res) => {
+    res.json({
+        name: "Produc 1",
+        price: 1000
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`)
+});
