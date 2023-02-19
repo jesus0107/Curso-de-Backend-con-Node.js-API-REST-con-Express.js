13367ee859919c4d92dbd46f342e6d9c964b0321
jesus <bep010795@gmail.com>
2023-02-18 19:18:19 -0600

    Rutas get y parametros de ruta

--- index.js
+++ index.js
@@ -5,17 +5,35 @@ const port =  3000
 app.get("/", (req, res) => {
     res.send("Primer servidor en express");
 })
-
-app.get("/nueva-ruta", (req, res) => {
-    res.send("Otra ruta");
+// ----- Get all products
+app.get("/products", (req, res) => {
+    res.json([
+        {
+            name: "Produc 1",
+            price: 1000
+        },
+        {
+            name: "Produc 1",
+            price: 1000
+        },
+        {
+            name: "Produc 1",
+            price: 1000
+        },
+    ]);
 })
 
-app.get("/enviando-json", (req, res) => {
+// ---- Get a product
+app.get("/products/:id", (req, res) => {
+    const { id } = req.params;
     res.json({
+        id,
         name: "Produc 1",
         price: 1000
     });
 })
+
+
 
 app.listen(port, () => {
     console.log(`Escuchando en el puerto ${port}`)
